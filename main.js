1da2b82c62a830ec676e410524b4ba1fcd28b783
Sebastian Ramos <jsramosanj@gmail.com>
2022-08-27 21:52:22 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,26 @@
+window.onload=function(){
+    const menuemail = document.querySelector('.navbar-email');
+    const desktopmenu = document.querySelector('.desktop-menu');
+
+
+    menuemail.addEventListener('click', toggledesktopmenu);
+    function toggledesktopmenu () {
+        desktopmenu.classList.toggle('inactive');
+      }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
